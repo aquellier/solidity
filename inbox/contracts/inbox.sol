b0b33da7ca89b7c0617921da5b49fc6654cb66d0
antoinequellier <antoinecquellier@gmail.com>
2018-11-14 14:09:36 +0000

    add pseudo random and pickwinner function in contract

--- inbox/contracts/inbox.sol
+++ inbox/contracts/inbox.sol
@@ -10,4 +10,13 @@ contract Inbox {
     function setMessage(string newMessage) public {
         message = newMessage;
     }
+
+    function random() private view returns (uint) {
+        return uint(sha3(block.difficulty, now, players));
+    }
+
+    function pickWinner() public {
+        uint index = random() % players.length;
+        players[index].transfer(this.balance);
+    }
 }
